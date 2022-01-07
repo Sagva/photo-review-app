d79b30c38081776b8f06486c39af25e2444f12f8
Elena Myadzeleva <sagva2014@gmail.com>
2022-01-07 22:49:06 +0100

    Added SignupPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import SignupPage from "./pages/SignupPage";
 function App() {
   return (
     <div className="App">
       Foto review app
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
     </div>
   );
 }
--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,4 +1,11 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  updateProfile,
+} from "firebase/auth";
+import { auth, db } from "../firebase";
+import { doc, setDoc } from "firebase/firestore";
 
 const AuthContext = createContext();
 
@@ -7,9 +14,33 @@ const useAuthContext = () => {
 };
 
 const AuthContextProvider = ({ children }) => {
-  const values = {};
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+  const signup = (email, password) => {
+		return createUserWithEmailAndPassword(auth, email, password)
+	}
+
+  useEffect(() => {
+    // listen for auth-state changes
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+  }, []);
+
+  const values = {
+    currentUser,
+    loading,
+    signup,
+  };
+
+  return (
+    <AuthContext.Provider value={values}>
+      {loading && <div>Loading...</div>}
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 };
 
 export { useAuthContext, AuthContextProvider as default };
