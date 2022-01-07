93bb34883e996849f597f6a70951abd95e2a4ce4
Elena Myadzeleva <sagva2014@gmail.com>
2022-01-07 22:24:13 +0100

    Added BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import Home from "./pages/Home";
 function App() {
-  return <div className="App">Foto review app</div>;
+  return (
+    <div className="App">
+      Foto review app
+      <Home />
+    </div>
+  );
 }
 
 export default App;
