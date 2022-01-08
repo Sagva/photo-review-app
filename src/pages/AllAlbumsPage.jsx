137641035b6c8eb4fff727bcf8b45e41fad2c85b
Elena Myadzeleva <sagva2014@gmail.com>
2022-01-08 06:28:39 +0100

    Fetched all albums

--- src/pages/AllAlbumsPage.jsx
+++ src/pages/AllAlbumsPage.jsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "react-bootstrap";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, query, where } from "firebase/firestore";
 import { db } from "../firebase";
 import { useAuthContext } from "../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { useFirestoreQueryData } from "@react-query-firebase/firestore";
 
 const AllAlbumsPage = () => {
   const { currentUser } = useAuthContext();
   const navigate = useNavigate();
 
+  const queryRef = query(collection(db, "albums"));
+
+  const { data, isLoading } = useFirestoreQueryData(
+    ["albums"],
+    queryRef,
+    { idField: "id", subscribe: true },
+    { fetchOnMount: "always" }
+  );
+
   const handleClick = async () => {
     const newAlbum = await addDoc(collection(db, "albums"), {
       name: "New album",
-      accessList: [currentUser.uid],
+      owner: currentUser.uid,
     });
 
     navigate(`/album/${newAlbum.id}`);
   };
 
+  useEffect(() => {
+    console.log(`data`, data);
+  }, [data]);
   return (
     <div>
       <h3 className="my-4">Allalbums page</h3>
@@ -25,6 +38,21 @@ const AllAlbumsPage = () => {
       <Button variant="secondary" onClick={handleClick}>
         Create New Album
       </Button>
+
+      {isLoading && <p>isLoading...</p>}
+      {data && (
+        <>
+          {data.length ? (
+            <div>
+              {data.map((album) => {
+                return <div key={album.id}>{album.id}</div>;
+              })}
+            </div>
+          ) : (
+            <p>You don't have any albums yet</p>
+          )}
+        </>
+      )}
     </div>
   );
 };
