6f36ad3b5274c9d72f77248a4287255a60fd3b21
Elena Myadzeleva <sagva2014@gmail.com>
2022-01-10 14:05:52 +0100

    Added thumbsUp and thumbsDown functions

--- src/components/ImageGid.jsx
+++ src/components/ImageGid.jsx
@@ -1,19 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Button, Col, Row } from "react-bootstrap";
 import { useAuthContext } from "../contexts/AuthContext";
 import UseCreateNewAlbum from "../hooks/UseCreateNewAlbum";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp, faThumbsDown } from "@fortawesome/free-solid-svg-icons";
+
 const ImageGrid = ({ urls }) => {
   const [chosenPhotos, setChosenPhotos] = useState([]);
+  const [photosToDelete, setPhotosToDelete] = useState([]);
   const { currentUser } = useAuthContext();
   const createNewAlbum = UseCreateNewAlbum();
 
   const toggleChosenPhoto = (photosUrl) => {
     if (chosenPhotos.includes(photosUrl)) {
       setChosenPhotos(chosenPhotos.filter((url) => url !== photosUrl));
-      chosenPhotos.filter((url) => url !== photosUrl);
     } else if (!chosenPhotos.includes("photosUrl")) {
       setChosenPhotos([...chosenPhotos, photosUrl]);
+      setPhotosToDelete(photosToDelete.filter((url) => url !== photosUrl));
+    }
+  };
+
+  const toggleDislikePhoto = (photosUrl) => {
+    if (photosToDelete.includes(photosUrl)) {
+      setPhotosToDelete(photosToDelete.filter((url) => url !== photosUrl));
+    } else if (!photosToDelete.includes("photosUrl")) {
+      setPhotosToDelete([...photosToDelete, photosUrl]);
+      setChosenPhotos(chosenPhotos.filter((url) => url !== photosUrl));
     }
   };
 
@@ -28,9 +41,9 @@ const ImageGrid = ({ urls }) => {
             >
               <div className="ImageBox">
                 <img
-                  className={
-                    chosenPhotos.includes(url) ? "chosen" : "not-chosen"
-                  }
+                  className={`image ${
+                    chosenPhotos.includes(url) ? "chosen" : ""
+                  } ${photosToDelete.includes(url) ? "disliked" : ""}`}
                   src={url}
                   alt={url}
                   style={{
@@ -39,32 +52,61 @@ const ImageGrid = ({ urls }) => {
                     cursor: "pointer",
                   }}
                 />
-                <input
-                  className="chosePhotoCheckbox"
-                  type="checkbox"
-                  onChange={() => toggleChosenPhoto(url)}
-                  style={{
-                    cursor: "pointer",
-                  }}
-                />
+                {currentUser && (
+                  <input
+                    className="chosePhotoCheckbox"
+                    type="checkbox"
+                    onChange={() => toggleChosenPhoto(url)}
+                    style={{
+                      cursor: "pointer",
+                    }}
+                  />
+                )}
+                {!currentUser && (
+                  <div className="mt-3">
+                    <button onClick={() => toggleChosenPhoto(url)}>
+                      <FontAwesomeIcon icon={faThumbsUp} />
+                    </button>
+                    <button onClick={() => toggleDislikePhoto(url)}>
+                      <FontAwesomeIcon icon={faThumbsDown} />
+                    </button>
+                  </div>
+                )}
               </div>
             </Col>
           ))}
       </Row>
-      <Button
-        variant="secondary"
-        style={{ display: chosenPhotos.length ? "block" : "none" }}
-        onClick={() =>
-          createNewAlbum(
-            "New album",
-            chosenPhotos,
-            "here should be photographer's id"
-          )
-        }
-        className="my-5"
-      >
-        Create new album
-      </Button>
+      {currentUser ? (
+        <Button
+          variant="secondary"
+          style={{ display: chosenPhotos.length ? "block" : "none" }}
+          onClick={() =>
+            createNewAlbum(
+              "New album",
+              chosenPhotos,
+              "here should be photographer's id"
+            )
+          }
+          className="my-5"
+        >
+          Create new album
+        </Button>
+      ) : (
+        <Button
+          variant="secondary"
+          // style={{ display: chosenPhotos.length ? "block" : "none" }}
+          onClick={() =>
+            createNewAlbum(
+              "New album",
+              chosenPhotos,
+              "here should be photographer's id"
+            )
+          }
+          className="my-5"
+        >
+          Send to Photographer
+        </Button>
+      )}
     </div>
   );
 };
