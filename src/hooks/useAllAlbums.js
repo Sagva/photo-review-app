b74c60b0069badc2eeb8cbfea96fe484f76c1220
Elena Myadzeleva <sagva2014@gmail.com>
2022-01-12 14:49:59 +0100

    Added timpstamp to the new album ans sorting by timestamp on All albums page

--- src/hooks/useAllAlbums.js
+++ src/hooks/useAllAlbums.js
@@ -1,25 +1,25 @@
-import { useFirestoreQueryData } from '@react-query-firebase/firestore';
-import { collection, query, where } from 'firebase/firestore';
-import { useAuthContext } from '../contexts/AuthContext';
-import { db } from '../firebase';
-
+import { useFirestoreQueryData } from "@react-query-firebase/firestore";
+import { collection, orderBy, query, where } from "firebase/firestore";
+import { useAuthContext } from "../contexts/AuthContext";
+import { db } from "../firebase";
 
 const useAllAlbums = () => {
-    const { currentUser } = useAuthContext();
+  const { currentUser } = useAuthContext();
 
-    const queryRef = query(
-        collection(db, "albums"),
-        where("owner", "==", currentUser.uid)
-      );
-    
-      const { data, isLoading } = useFirestoreQueryData(
-        ["albums", currentUser.uid],
-        queryRef,
-        { idField: "id", subscribe: true },
-        { refetchOnMount: "always" }
-      );
+  const queryRef = query(
+    collection(db, "albums"),
+    where("owner", "==", currentUser.uid),
+    orderBy("timestamp")
+  );
 
-    return {data, isLoading};
-}
- 
+  const { data, isLoading } = useFirestoreQueryData(
+    ["albums", currentUser.uid],
+    queryRef,
+    { idField: "id", subscribe: true },
+    { refetchOnMount: "always" }
+  );
+
+  return { data, isLoading };
+};
+
 export default useAllAlbums;
